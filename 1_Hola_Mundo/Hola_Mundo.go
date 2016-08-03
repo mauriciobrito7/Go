9328969d050bf5a112a33335a6bb3f62b21af27b
Mauricio Eduardo <maubot01@gmail.com>
2016-08-02 19:51:43 -0430

    Hola Mundo

---
+++ 1_Hola_Mundo/Hola_Mundo.go
@@ -0,0 +1,8 @@
+//Todo programa en go debe tener un package main
+package main;
+//Importar la libreria para imprimir por consola
+import "fmt";
+
+func main(){
+	fmt.Println("Hello World");// famoso hola mundo
+}
