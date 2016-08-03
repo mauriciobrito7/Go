362aa9b8270795148017227a89cee94357b9cdec
Mauricio Eduardo <maubot01@gmail.com>
2016-08-03 16:03:47 -0430

    For

---
+++ 13_For/For.go
@@ -0,0 +1,21 @@
+package main;
+
+import "fmt";
+
+func main(){
+	//for clasico
+	for j:=7;j<10;j++{
+		fmt.Println(j);
+	}
+	//simulando un while
+	i:=1;
+	for i<10{
+		fmt.Println(i);	
+	}
+
+	for{
+		fmt.Println("loop");
+		break;	
+	}
+
+}
