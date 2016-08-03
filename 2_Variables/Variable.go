05d3b08c51abb07a84963b41c92b69bf5b7aad0d
Mauricio Eduardo <maubot01@gmail.com>
2016-08-02 20:21:48 -0430

    Variables

---
+++ 2_Variables/Variable.go
@@ -0,0 +1,17 @@
+package main;
+
+import "fmt";
+
+func main(){
+	//var nombre_variable tipo_dato;
+	//Todas las variables estan iniciadas en el caso de los números 0
+	var x int;
+	//Segunda forma de declar una variable
+	y:=23;// no se necesita declarar el tipo de la variable
+	//En el caso de los string tiene una cadena vacia
+	var cadena string;
+	//En el caso de los booleanos es falso
+	var bandera bool;
+
+	fmt.Println(y);
+}
